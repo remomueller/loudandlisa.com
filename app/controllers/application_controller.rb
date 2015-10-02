b74d73db2efcd356e63adf3db06c108712aed0a2
Remo Mueller <remosm@gmail.com>
2015-10-01 22:23:19 -0400

    Added Code Climate configuration file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# Main web application class that hosts static and public facing pages
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
