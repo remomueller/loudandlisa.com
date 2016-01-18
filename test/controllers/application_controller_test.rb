912196b79cb7e7ddec9ce05ff94bea07eea7a105
Remo Mueller <remosm@gmail.com>
2016-01-18 12:05:33 -0500

    Cleaned up tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,38 +1,34 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionController::TestCase
-  setup do
-  end
-
-  test "should get home as logged out user" do
+  test 'should get home as public user' do
     get :home
     assert_response :success
   end
 
-  test "should get demos as logged out user" do
+  test 'should get demos as public user' do
     get :demos
     assert_response :success
   end
 
-  test "should get services as logged out user" do
+  test 'should get services as public user' do
     get :services
     assert_response :success
   end
 
-  test "should get contact as logged out user" do
+  test 'should get contact as public user' do
     get :contact
     assert_response :success
   end
 
-  test "should submit contact as logged out user" do
-    post :submit_contact, name: 'Name', email: 'test@example.com', body: 'Line 1\nLine 2'
+  test 'should submit contact as public user' do
+    post :submit_contact, name: 'Name', email: 'test@example.com', body: "Line 1\nLine 2"
     assert_redirected_to contact_path
   end
 
-  test "should not submit contact as logged out user with missing fields" do
+  test 'should not submit contact as public user with missing fields' do
     post :submit_contact, name: '', email: '', body: ''
     assert_response :success
     assert_template 'contact'
   end
-
 end
