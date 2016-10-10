71972520a3ec2c85387389e53f75125e15fef34b
Remo Mueller <remosm@gmail.com>
2016-10-10 11:48:11 -0400

    Don't force SSL connection

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_www_loudandlisa_com_session',
-                                                      secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_www_loudandlisa_com_session'
