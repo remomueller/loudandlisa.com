2044cbd4bcafc94489f5870943e2c10c0730d266
Remo Mueller <remosm@gmail.com>
2017-05-20 12:35:39 -0400

    Version bump to 3.0.0.pre

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## 3.0.0
+
 ## 2.0.0 (October 10, 2016)
 
 ### Enhancements
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,11 +2,11 @@
 
 module WwwLoudandlisaCom
   module VERSION #:nodoc:
-    MAJOR = 2
+    MAJOR = 3
     MINOR = 0
     TINY = 0
-    BUILD = nil # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = nil # "pre", "beta1", "beta2", "rc", "rc2", nil
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
   end
 end
